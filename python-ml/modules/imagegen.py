d420ac602c37d095ebfa9a2e2168bf76df818e55
Ryan Abraham <ryan_ja@live.ca>
2017-11-05 01:05:12 -0500

    Comments and bugfixes

--- python-ml/modules/imagegen.py
+++ python-ml/modules/imagegen.py
@@ -6,20 +6,30 @@ import modules.config as config
 # Connect API
 da = deviantart.Api(config.client_id, config.client_secret)
 
+# Provides a DeviantArt image URL related to a cardname
 def generate_image_url(cardname):
     deviations = {'results': []}
     tag_ = ""
+    # For each word in the cardname, try and find a relevant DeviantArt tag
     for word in reversed(re.findall(r"[\w']+", cardname)):
         tags = da.search_tags(word)
         if(tags != []):
             tag_ = random.choice(tags)
     if(tag_ == ""):
+        # No tag was found, so pick some random deviations
         while(deviations['results'] == []):
             deviations = da.browse(endpoint='popular')
     else:
+        # Browse deviations of that tag
         deviations = da.browse(endpoint='tags', tag=tag_)
 
-    image = random.choice(deviations['results'])
-    return image.preview['src']
+    # Select a random deviation
+    deviation = random.choice(deviations['results'])
+    while(deviation.preview == None):
+        # No preview, therefore the deviation is a written story.
+        # Repick a new deviation until an image deviation is picked.
+        deviation = random.choice(deviations['results'])
 
+    # Return image URL
+    return deviation.preview['src']
 
