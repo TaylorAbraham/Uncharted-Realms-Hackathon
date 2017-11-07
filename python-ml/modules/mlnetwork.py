950ea321a426beb885fd21021d814f0b1b13a1db
Ryan Abraham <ryan_ja@live.ca>
2017-11-07 17:40:29 -0500

    ML cleanup

--- python-ml/modules/mlnetwork.py
+++ python-ml/modules/mlnetwork.py
@@ -14,6 +14,7 @@ def predict_costs(card_dict):
     # First, convert the EFF list to one hot
     df = pd.concat([df, pd.get_dummies(df['EFF'])], axis=1)
     df = df.drop(['EFF'], axis=1) # Drop the old EFF feature
+    print(df)
     #print(df)
     X = df.drop(['CLK'], axis=1)
     y = df.CLK
@@ -21,7 +22,7 @@ def predict_costs(card_dict):
     # Create linear regression and fit it on the pre-existing data
     # This is used for accuracy evaluation later on
     lr_clf = LinearRegression()
-    lr_clf.fit(X.drop(['Charge', 'Ward']), y)
+    lr_clf.fit(X.drop(['Charge', 'Ward'], axis=1), y)
     ## Create random forest and fit it on the pre-existing data
     rfc = RandomForestClassifier(n_jobs=-1,max_features= 'sqrt' , n_estimators=70, oob_score = True, random_state=2)
     rfc.fit(X, y)
@@ -37,8 +38,9 @@ def predict_costs(card_dict):
     # Predict costs for each of the cards
     preds = rfc.predict(cards.drop(['NAME', 'IMG'], axis=1))
 
-    print("\nLinear Regression Score")
-    lin_preds = lr_clf.predict(cards.drop(['NAME', 'IMG'], axis=1))
+    print("------------------------")
+    print("Linear Regression Score")
+    lin_preds = lr_clf.predict(cards.drop(['NAME', 'IMG', 'Charge', 'Ward'], axis=1))
     lin_preds = np.rint(lin_preds)
     from sklearn.metrics import accuracy_score
     acc_score = accuracy_score(lin_preds, preds)
